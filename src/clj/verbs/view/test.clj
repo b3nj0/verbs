95dc4ee05f2bf5abcd15baa16cfbac0ef001e529
Benjamin Jenks <jenksb@gmail.com>
2013-05-05 15:16:58 +0100

    test: start adding basic page structure

--- src/clj/verbs/view/test.clj
+++ src/clj/verbs/view/test.clj
@@ -1,9 +1,16 @@
 (ns verbs.view.test
-  (:use [hiccup.page])
+  (:use [hiccup element page])
   (:require [verbs.view.common :as common]))
 
 (defn test []
   (common/scaffold {:includes (include-js "/cljs/verb-test.js")}
                    [:div.row
-                    [:div#content.span8]
-                    [:div#settings.span4]]))
+                    [:div#content.span9]
+                    [:div#settings.span3
+                     [:ul.nav.nav-list
+                      [:li.nav-header "Verb Tests"]
+                      [:li (link-to "#" "Conjugate")]
+                      [:li (link-to "#" "Name the verb")]
+                      [:li.nav-header "Settings"]
+                      [:li (link-to "#" "Select verbs")]
+                      [:li (link-to "#" "Select tenses")]]]]))
