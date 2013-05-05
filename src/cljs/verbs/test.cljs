2cefb19c02dbf87ec975b67935fb35c51e956a1a
Benjamin Jenks <jenksb@gmail.com>
2013-05-05 16:14:36 +0100

    test.cljs: first event handler added

--- src/cljs/verbs/test.cljs
+++ src/cljs/verbs/test.cljs
@@ -1,7 +1,28 @@
 (ns verbs.test
-  (:require [goog.net.XhrIo :as xhr]))
+  (:use-macros
+   [dommy.macros :only [sel1]])
+  (:require [goog.net.XhrIo :as xhr]
+            [dommy.core :as dommy]))
+
+
+;; data
 
 (def verbs (atom {}))
 
-;; request and store all the verbs from the backend
-(xhr/send "/raw/verbs" (fn [e] (reset! verbs (.getResponseJson (.-target e)))))
+;; utils
+
+(defn log [v]
+  (.log js/console v))
+
+
+
+;; initialisation
+
+(defn init []
+  ;; request and store all the verbs from the backend.  then attach
+  (xhr/send "/raw/verbs" (fn [e] (reset! verbs (.getResponseJson (.-target e)))))
+
+  ;; wire up event handlers
+  (dommy/listen! (sel1 :#conjugate) :click (fn [e] (log "Clicked") (log e))))
+
+(set! (.-onload js/window) init)
