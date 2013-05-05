bf4e8f7841fe5547e1f5c5053e0a9238c329f518
Benjamin Jenks <jenksb@gmail.com>
2013-05-05 16:13:56 +0100

    test: give the menu anchors ids

--- src/clj/verbs/view/test.clj
+++ src/clj/verbs/view/test.clj
@@ -2,6 +2,9 @@
   (:use [hiccup element page])
   (:require [verbs.view.common :as common]))
 
+(defn lnk [id lbl]
+  [:a {:id id :href (str "#" (name id))} lbl])
+
 (defn test []
   (common/scaffold {:includes (include-js "/cljs/verb-test.js")}
                    [:div.row
@@ -9,8 +12,8 @@
                     [:div#settings.span3
                      [:ul.nav.nav-list
                       [:li.nav-header "Verb Tests"]
-                      [:li (link-to "#" "Conjugate")]
-                      [:li (link-to "#" "Name the verb")]
+                      [:li (lnk :conjugate "Conjugate")]
+                      [:li (lnk :name-the-verb "Name the verb")]
                       [:li.nav-header "Settings"]
-                      [:li (link-to "#" "Select verbs")]
-                      [:li (link-to "#" "Select tenses")]]]]))
+                      [:li (lnk :select-verbs "Select verbs")]
+                      [:li (lnk :select-tenses "Select tenses")]]]]))
