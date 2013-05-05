e748dd3e7a98d9d5d5b47b10863f778db1349cf8
Benjamin Jenks <jenksb@gmail.com>
2013-05-05 13:38:30 +0100

    test.cljs: request and store all the verbs from the backend

--- src/cljs/verbs/test.cljs
+++ src/cljs/verbs/test.cljs
@@ -1,8 +1,7 @@
 (ns verbs.test
   (:require [goog.net.XhrIo :as xhr]))
 
-(defn display-results [e]
-  (let [json (.getResponseJson (.-target e))]
-    (.log js/console json)))
+(def verbs (atom {}))
 
-(xhr/send "/raw/verbs" display-results)
+;; request and store all the verbs from the backend
+(xhr/send "/raw/verbs" (fn [e] (reset! verbs (.getResponseJson (.-target e)))))
